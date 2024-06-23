dc26790a8d2ebe13f5b339002c989d457c03940e
GabrielDiasGomesProgramer <161972832+GabrielDiasGomesProgramer@users.noreply.github.com>
2024-06-23 19:08:48 -0300

    atualização de slider ainda não finalizado

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,19 +1,23 @@
 import './Menu.css'
 import slider from '../../assets/slider.png'
-
+import { useState } from 'react'
+import Openbtn from '../openbtn/Openbtn';
+import sliderlogo from '../../assets/kotpota.png';
 function Menu() {
+  const [buttonPopup, setButtonPopup] =useState(false);
   return (
 
     <>
     
     <div className="main-of-menu">
     <h1>Novidade:</h1>
-    <a href="">
+   
     <div className="menu">
-    <img src={slider} alt='slider' />
-    </div></a>
+    <button onClick={() => setButtonPopup(true)}>
+    <img src={slider} alt='slider' /></button>
     </div>
-
+    </div>
+    <Openbtn trigger={buttonPopup} setTrigger={setButtonPopup} capadefilme={slider} logodefilme={sliderlogo} sinopsedefilme='a' classificadefilme='A14' gedefilme='Ação | Aventura | Fantasia | Ficção-Ciêntifica'/>
       
     </>
   );
